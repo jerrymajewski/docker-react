e937b4e1dd4ee00637c58a14db948ce7f1a6b54e
jerrymajewski <jerry.majewski@am.jll.com>
2020-04-25 20:35:52 +0530

    Change test message.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST 4
+          CRA Test
         </p>
         <a
           className="App-link"
